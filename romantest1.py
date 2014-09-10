8e6d1f5a6f610a709828bdbf234e3b5cba60f0b4
Luis <luis.lopez@cimat.mx>
2014-09-10 01:42:53 -0500

    Correcciones
    Correcciones en el archivo de pruebas

--- romantest1.py
+++ romantest1.py
@@ -101,19 +101,19 @@ class RoundTripCheck (unittest.TestCase):
 class FromRomanBadInput(unittest.TestCase):
     def test_too_many_repeated_numerals(self):
         '''from_roman should fail with too many repeated numerals'''
-        for s in ('MMMM','DD'. 'CCCC','LL', 'XXXX', 'VV', 'IIII'):
-            self.assertRaises(roman1.InvalidRomanNumeralError, romain1.from_roman, s)
+        for s in ('MMMM','DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII'):
+            self.assertRaises(roman1.InvalidRomanNumeralError, roman1.from_roman, s)
 
     def test_repeated_pairs(self):
         '''from_roman sould fail with repeated pairs of numerals'''
         for s in ('CMCM', 'CDCD', 'XCXC', 'XLXL', 'IXIX', 'IVIV'):
-            self.assertRaises(roman1.InvalidRomanNumeralError, romain1.from_roman, s)
+            self.assertRaises(roman1.InvalidRomanNumeralError, roman1.from_roman, s)
 
     def test_malformed_antecedents(self):
         '''from_roman sould fail with malformed antecedents'''
         for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV',
                   'MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
-            self.assertRaises(roman1.InvalidRomanNumeralError, romain1.from_roman, s)
+            self.assertRaises(roman1.InvalidRomanNumeralError, roman1.from_roman, s)
 
 if __name__ == '__main__':
     unittest.main()
