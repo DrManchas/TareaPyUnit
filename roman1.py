5bf182b566e9495b838b6a1ef178ffc5c37baf04
Luis <luis.lopez@cimat.mx>
2014-09-10 01:00:16 -0500

    Romano Falla
    método vacio que falla y la prueba

---
+++ roman1.py
@@ -0,0 +1,20 @@
+'''Convert to and from Roman numerals'''
+
+roman_numeral_map = (('M',  1000),
+                     ('CM', 900),
+                     ('D',  500),
+                     ('CD', 400),
+                     ('C',  100),
+                     ('XC', 90),
+                     ('L',  50),
+                     ('XL', 40),
+                     ('X',  10),
+                     ('IX', 9),
+                     ('V',  5),
+                     ('IV', 4),
+                     ('I',  1))
+
+def to_roman(n):
+    '''convert integer to Roman numeral'''
+    result = ''
+    pass
